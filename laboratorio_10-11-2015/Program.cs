ade130b51e16700a04208e7a46218993aae5e447
diogo <diogo.j.menezes@gmail.com>
2015-11-10 12:31:01 -0200

    metodo retorna todas as posicoes com 0

---
+++ laboratorio_10-11-2015/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace laboratorio_10_11_2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[20];
+            Random ale = new Random();
+            classVetor v = new classVetor();
+
+            for (int i = 0; i < vetor.Length; i++) {
+                vetor[i] = ale.Next(10);
+                Console.WriteLine(vetor[i]);
+            }
+            Console.WriteLine("\nMenor valor: " + v.procura_menor(vetor));
+            Console.WriteLine("Maior valor: " + v.procura_maior(vetor));
+
+            Console.Write("\nDigite um número presente no vetor: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nO número " + numero + " apareceu primeiro na posição: " + v.primeira_posicao(vetor,numero));
+            Console.WriteLine("Todas as posições do número " + numero);
+            for (int j = 0; j < vetor.Length; j++) {
+                Console.WriteLine(v.todas_posicoes(vetor, numero)[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ laboratorio_10-11-2015/classVetor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace laboratorio_10_11_2015
+{
+    class classVetor
+    {
+        public int procura_menor(int[] v) {
+            int menor = 11;
+            for (int i = 0; i < v.Length; i++) {
+                if (v[i] < menor) {
+                    menor = v[i];
+                }
+            }
+            return menor;
+        }
+        public int procura_maior(int[] v) {
+            int maior = 0;
+            for (int i = 0; i < v.Length; i++) {
+                if (v[i] > maior) {
+                    maior = v[i];
+                }
+            }
+            return maior;
+        }
+        public int primeira_posicao(int[] v, int num) {
+            int posicao = 0;
+            for (int i = 0; i < v.Length; i++) {
+                if (num == v[i]) {
+                    posicao = i;
+                    i = v.Length;
+                }
+            }
+            return posicao;
+        }
+        public int[] todas_posicoes(int[] v, int num) {
+            int[] posicao = new int[20];
+            for (int i = 0; i < v.Length; i++) {
+                if (num == v[i])
+                {
+                    posicao[i] = i;
+                }
+            }
+            return posicao;
+        }
+    }
+}
